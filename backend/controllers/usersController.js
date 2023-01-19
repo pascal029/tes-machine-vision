3fa6457280b6418b8948d4678417ed566d1427f2
pascal029 <pascalvirgiawan05@gmail.com>
2023-01-19 22:03:40 +0700

    add routes user(getbyid, put data, change password)

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,7 @@
+class UserController {
+  static async getById(req, res, next) {}
+  static async editUser(req, res, next) {}
+  static async changePassword(req, res, next) {}
+}
+
+module.exports = UserController;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const usersAuth = require("./userAuth");
 const posts = require("./posts");
+const users = require("./users");
 
 router.get("/", (req, res, next) => {
   res.status(200).json({ msg: "Hello world!" });
@@ -8,5 +9,6 @@ router.get("/", (req, res, next) => {
 
 router.use("/auth", usersAuth);
 router.use("/posts", posts);
+router.use("/user", users);
 
 module.exports = router;
